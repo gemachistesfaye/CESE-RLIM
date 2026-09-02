15d8819bfa0a699e128c062754188687be6c51b0
gemachistesfaye <gemachistesfaye36@gmail.com>
2026-09-02 15:16:35 +0300

    feat(api): make AuditModule globally available

--- apps/api/src/audit/audit.module.ts
+++ apps/api/src/audit/audit.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuditService } from './audit.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
+@Global()
 @Module({
   imports: [PrismaModule],
   providers: [AuditService],
